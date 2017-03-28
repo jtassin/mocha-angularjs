adc3dc8143a6ee004b899616bc2bbb225c15a7d8
Julien Tassin <jtassin@artprice.com>
2017-03-28 11:12:06 +0200

    problem solved

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,39 @@
-import expect from 'chai';
-import getFive from './index';
+import 'jsdom-global/register';
+import { expect } from 'chai';
+
+
+before(function(done) {
+var angularcontext = require('angularcontext');
+var context = angularcontext.Context();
+context.runFile(
+    './node_modules/angular/angular.js',
+    function (result, error) {
+      if (error) {
+        console.error(error);
+        done(error);
+      }
+      else {
+        global.angular = context.getAngular();
+        console.log(3, context.getAngular(), 4);
+        done();
+        // can now use other methods of the context
+      }
+    }
+);
+});
+
+// global.angular = 54;
+
+
+
+
+
 
 describe('desc', () => {
   it('my test', () => {
+  const getFive = require('./index').default;
+  console.log(1, global.angular, 2);
+  console.log(getFive()+10);
     expect(getFive()).to.equal(5);
   });
 });
